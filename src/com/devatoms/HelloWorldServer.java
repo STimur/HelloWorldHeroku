6dbf2d84dfc5e9ab9fbdc9959faf10a3e8a362ca
Timur <timur.salakhetdinov@gmail.com>
2018-07-06 11:47:51 +0300

    new requirement is to change lang to Eesti

--- src/com/devatoms/HelloWorldServer.java
+++ src/com/devatoms/HelloWorldServer.java
@@ -3,21 +3,17 @@ package com.devatoms;
 import com.google.gson.Gson;
 
 import static spark.Spark.get;
-import static spark.Spark.setPort;
+import static spark.Spark.port;
 
 public class HelloWorldServer {
   public static void main(String[] args) {
-    ProcessBuilder process = new ProcessBuilder();
-    Integer port;
-    // This tells our app that if Heroku sets a port for us, we need to use that port.
-    // Otherwise, if they do not, continue using port 4567.
-    if (process.environment().get("PORT") != null) {
-      port = Integer.parseInt(process.environment().get("PORT"));
-    } else {
-      port = 4567;
-    }
-    setPort(port);
-
+    setPortForHeroku();
     get("/hello", (req, res) -> new Gson().toJson(HelloWorld.say()));
+  }
+
+  private static void setPortForHeroku() {
+    String port = new ProcessBuilder().environment().get("PORT");
+    if (port != null)
+      port(Integer.parseInt(port));
   }
 }
